81d280e9439762f25845b938d7b71934e54aa60f
sikanderc <sikanderchowhan@gmail.com>
2017-09-15 09:15:49 -0400

    Done.

--- db/migrate/01_create_songs.rb
+++ db/migrate/01_create_songs.rb
@@ -1,2 +1,10 @@
 class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :name
+      t.string :artist
+      t.integer :genre_id
+      t.integer :artist_id
+    end
+  end
 end
